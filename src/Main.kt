70a2511b305c9ca491595f0cd2492d8e1969885d
Joe Davidson <jdavidson@students.mchenry.edu>
2024-01-22 18:50:19 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    val myInt = 15
+    val myDouble = 250.432
+    val myFloat = 10.2f
+    val myLong = 1000000004
+    val myShort = -30222
+    val myByte = 110
+    val myletter: Char
+    myletter = 'A'
+    val myBoolean = true
+
+
+    println("Int Value is " + myInt)
+    println("Double Value is " + myDouble)
+    println("Float Value is " + myFloat)
+    println("Long Value is " + myLong)
+    println("Short Value is " + myShort)
+    println("Byte Value is " + myByte)
+    println("Char Letter is " + myletter)
+    println("Boolean is " + myBoolean)
+
+}
